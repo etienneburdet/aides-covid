658a5533d537e0ae5a0881e41c2bb63323cf145d
Etienne Burdet <etienne.burdet@gmail.com>
2020-05-02 17:58:53 +0200

    basic fetch with axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,34 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
         aides-covid
       </h1>
       <h2 class="subtitle">
         Replication of official app on ODS
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <pre>
+        {{ geoData }}
+      </pre>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+// import axios from 'axios'
+const baseUrL = 'https://eburdet.opendatasoft.com/api/v2/catalog/datasets/no-name/'
 
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      geoData: 'Salut'
+    }
+  },
+  async asyncData ({ $axios }) {
+    const query = 'exports/geojson'
+    const geoData = await $axios.$get(baseUrL + query)
+    // const geoData = 'Yo'
+    return { geoData }
   }
 }
 </script>
