97b5f2f92be261f39b01a08a106884f9a3e1feda
Etienne Burdet <etienne.burdet@gmail.com>
2020-05-04 14:43:12 +0200

    add kpis

---
+++ components/KpiCard.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="text-center">
+    <p>{{ title }}</p>
+    <h5><strong>{{ kpi }}</strong></h5>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    kpi: Number
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
   <b-container fluid>
-    <b-row no-gutters>
+    <b-row>
       <b-col lg="4">
-        <Graph :raw-data="sectorData.aggregations" />
+        <h3 class="text-center">Totales aides versées en France</h3>
+        <div class="d-flex justify-content-around border-bottom border-grey py-4">
+          <KpiCard :kpi="totals.aggregations[0].total" title="montant" />
+          <KpiCard :kpi="totals.aggregations[0].count" title ="nombre d'aides" />
+        </div>
+        <h5 class="text-center mt-4">Top 10 des aides par secteur</h5>
+        <Graph :raw-data="sectorData.aggregations" class="border-bottom border-grey" />
+        <h4 class="text-center mt-4"><strong>Informations</strong></h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </b-col>
       <b-col lg="8">
         <Map :geo-data="geoData" />
@@ -14,13 +22,15 @@
 <script>
 import Map from '~/components/Map.vue'
 import Graph from '~/components/Graph.vue'
+import KpiCard from '~/components/KpiCard.vue'
 
 const baseUrl = 'https://eburdet.opendatasoft.com/api/v2/catalog/datasets/no-name/'
 
 export default {
   components: {
     Map,
-    Graph
+    Graph,
+    KpiCard
   },
   async asyncData ({ $axios }) {
     const queryGeo = 'exports/geojson'
@@ -29,45 +39,13 @@ export default {
     const querySector = 'aggregates?select=sum(montant_total) as sum_montant&group_by=libelle_section&sort=-sum_montant&limit=10'
     const sectorData = await $axios.$get(baseUrl + querySector)
 
-    return { geoData, sectorData }
-  },
-  data () {
-    return {
-      geoData: {}
-    }
+    const queryTotal = 'aggregates?select=count(*) as count, sum(montant_total) as total'
+    const totals = await $axios.$get(baseUrl + queryTotal)
+
+    return { geoData, sectorData, totals }
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
